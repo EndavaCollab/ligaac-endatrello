9e9ee9d614fd1e7a5cbb32cc438204a11c665799
catalin.halic@endava.com <catalin.halic@endava.com>
2023-04-21 16:16:42 +0300

    Setup Express and AngularMaterial

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const databaseCondig = require("./configs/db");
+
+app.disable("x-powered-by");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.disable("etag");
+
+app.use((res, req, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Controll-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Controll-Allow-Methods",
+      "PUT, POST, PATCH, DELETE, GET"
+    );
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+databaseCondig.connect();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(2323, () => {
+  console.log(`Example app listening on port 2323`);
+});
