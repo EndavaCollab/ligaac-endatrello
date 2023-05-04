4f41c6a3ef8535c496c42f06b58ec3a70ffae513
razalberto <raz_alberto@yahoo.com>
2023-05-04 23:07:39 +0300

    Added a toolbar in the dashboard view

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../shared';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, MatToolbarModule, MatIconModule, MatButtonModule],
 })
 export class DashboardModule {}
