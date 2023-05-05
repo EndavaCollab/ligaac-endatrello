524742c96fc28dc8dd891b1b4f52fd86f6e37139
goran-cds <codausigoran12@gmail.com>
2023-05-05 12:18:49 +0300

    ticket cards

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { IColumn } from '../shared';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-
+  columns: IColumn[] = [
+    {
+      title: 'To do',
+      tickets: [{ name: 'TicketA' }, { name: 'TicketB' }],
+    },
+    {
+      title: 'In progress',
+      tickets: [],
+    },
+    {
+      title: 'In testing',
+      tickets: [],
+    },
+    {
+      title: 'Done',
+      tickets: [],
+    },
+  ];
 }
