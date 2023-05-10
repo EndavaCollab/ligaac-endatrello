7d9e7e8e3284d0241c8fdeb35c7f5c6dbd7ffc4f
danabosoanca <dana_bosoanca>
2023-05-10 21:37:39 +0300

    Created the ticket-card component

---
+++ frontend/src/app/shared/ticket-card/ticket-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { ITicket } from '../interfaces';
+
+@Component({
+  selector: 'app-ticket-card',
+  templateUrl: './ticket-card.component.html',
+  styleUrls: ['./ticket-card.component.scss'],
+})
+export class TicketCardComponent {
+  @Input() ticket: ITicket = { name: '' };
+}
