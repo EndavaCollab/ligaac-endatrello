61747bed825b0a48e69a7e136f02ee17b5e10ef2
razalberto <raz_alberto@yahoo.com>
2023-05-11 22:07:10 +0300

    Progress cards and dashboard

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ColumnComponent } from './column/column.component';
+import { TicketCardComponent } from './ticket-card/ticket-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [CommonModule],
+  declarations: [ColumnComponent, TicketCardComponent],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [
+    CommonModule,
+    ColumnComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class SharedModule {}
