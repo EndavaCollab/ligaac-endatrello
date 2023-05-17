e7c7a59f350631ee2e48c66c8373dd913ebb9680
danabosoanca <dana_bosoanca>
2023-05-17 13:43:19 +0300

    Added methods for ticket

--- backend/tickets/controller.js
+++ backend/tickets/controller.js
@@ -5,3 +5,35 @@ const ticketModel = require("./model/ticket.model");
 exports.getAllTickets = (req, res, next) => {
   res.send("Get method for Tickets");
 };
+
+exports.getById = (req, res, next) => {
+  res.send("Get By Id Method Ticket");
+};
+
+exports.addTicket = async (req, res, next) => {
+  let body = req.body;
+
+  console.log(body);
+
+  const ticket = new ticketModel({
+    _id: new mongoose.Types.ObjectId(),
+    title: body.title,
+    description: body.description,
+  });
+
+  await ticket.save();
+
+  res.send("Add method for Tickets");
+};
+
+exports.deleteTicket = (req, res, next) => {
+  res.send("Delete method for Tickets");
+};
+
+exports.updateTicket = (req, res, next) => {
+  res.send("Update method for Tickets");
+};
+
+exports.patchTicket = (req, res, next) => {
+  res.send("Patch method for Tickets");
+};
--- backend/tickets/route.js
+++ backend/tickets/route.js
@@ -4,4 +4,14 @@ const controller = require("./controller");
 
 router.get("/", controller.getAllTickets);
 
+router.post("/", controller.addTicket);
+
+router.delete("/:id", controller.deleteTicket);
+
+router.put("/:id", controller.updateTicket);
+
+router.put("/:id", controller.patchTicket);
+
+router.get("/get-by-id/:id", controller.getById);
+
 module.exports = router;
