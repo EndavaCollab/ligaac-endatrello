5cf1dd4eb37d2e0873e7c18678530657b4db33a6
goran-cds <codausigoran12@gmail.com>
2023-05-19 11:30:02 +0300

    backend update

--- backend/tickets/controller.js
+++ backend/tickets/controller.js
@@ -3,41 +3,121 @@ const mongoose = require("mongoose");
 const ticketModel = require("./model/ticket.model");
 
 exports.getAllTickets = (req, res, next) => {
-  res.send("Get Method for Tickets");
+
+    ticketModel.find().then((results) => {
+        res.send({
+            status: 200,
+            message: "All tickets from database",
+            data: results
+        });
+    }, (error) => {
+        res.send({
+            status: 500,
+            message: "Something went wrong",
+            data: error
+        });
+    });
 };
 
 exports.getById = (req, res, next) => {
-  res.send("Get By Id Method for Ticket");
+
+    let ticketId = req.params.id;
+
+    console.log(ticketId);
+
+    ticketModel.findOne({ _id: ticketId }).then((result) => {
+        res.send({
+            status: 200,
+            message: "Get ticket with succes",
+            data: result
+        })
+    }, (error) => {
+        res.send({
+            status: 500,
+            message: "Something went wrong",
+            data: error
+        })
+    });
 };
 
 exports.addTicket = async (req, res, next) => {
-  let body = req.body;
+    let body = req.body;
 
-  console.log(body);
+    console.log(body);
 
-  const ticket = new ticketModel({
-    _id: new mongoose.Types.ObjectId(),
-    title: body.title,
-    description: body.description,
-  });
+    const ticket = new ticketModel({
+        _id: new mongoose.Types.ObjectId(),
+        title: body.title,
+        description: body.description,
+    });
 
-  await ticket.save();
+    ticket.save().then((result) => {
+        res.send({
+            status: 200,
+            message: "Add ticket with success",
+            data: result
+        })
+    }, (error) => {
+        res.send({
+            status: 500,
+            message: "Something went wrong",
+            data: error
+        })
+    });
 
-  res.send("Add Method for Ticket");
 };
 
 exports.deleteTicket = (req, res, next) => {
-  res.send("Delete Method for Ticket");
+
+    let ticketId = req.params.id;
+
+    console.log(ticketId);
+
+    ticketModel.deleteOne({ _id: ticketId }).then(() => {
+        res.send({
+            status: 200,
+            message: "Delete ticket with success",
+            data: null
+        })
+    }, (error) => {
+        res.send({
+            status: 500,
+            message: "Something went wrong",
+            data: error
+        })
+    });
 };
 
 exports.updateTicket = (req, res, next) => {
-  res.send("Update Method for Ticket");
-};
 
-exports.patchTicket = (req, res, next) => {
-  res.send("Patch Method for Ticket");
-};
+    let ticketId = req.params.id;
 
-exports.getById = (req, res, next) => {
-    res.send("GET BY ID Method for Ticket");
+    ticketModel
+        .updateOne(
+            { _id: ticketId },
+            {
+                $set: {
+                    _id: ticketId,
+                    title: req.body.title,
+                    description: req.body.description,
+                },
+            }
+        )
+        .then(
+            async () => {
+                let updatedTicket = await ticketModel.find({ _id: ticketId });
+                res.send({
+                    status: 200,
+                    message: "Update ticket with success!",
+                    data: updatedTicket[0],
+                });
+            },
+            (error) => {
+                res.send({
+                    status: 200,
+                    message: "Something went wrong!",
+                    data: error,
+                });
+            }
+        );
 };
