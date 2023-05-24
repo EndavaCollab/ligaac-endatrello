6932537b5732182b4a293039f30377d5e0ecd832
catalin-halic <catalin.halic@endava.com>
2023-05-24 13:35:34 +0300

    Add status for each ticket

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 exports.connect = () => {
   mongoose.connect(
-    "mongodb+srv://haliccatalin:<password>@endacluster.dervwc0.mongodb.net/EndaTrello?retryWrites=true;",
+    "mongodb+srv://haliccatalin:AHLQ3vx8YaTHK3Dw@endacluster.dervwc0.mongodb.net/EndaTrello?retryWrites=true;",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
