49d9e8bf4d0b8fef0f3a2435d2ff4c5edb5c0905
razalberto <raz_alberto@yahoo.com>
2023-06-08 22:11:59 +0300

    Live coding

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../shared';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogModule,
+} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, MatDialogModule],
 })
 export class DashboardModule {}
